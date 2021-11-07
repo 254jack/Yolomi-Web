c3e97e498d216d8784ae8227b4d635265914eceb
254jack <taixonj@gmail.com>
2021-11-07 23:48:09 +0300

    add initial to github

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#shop").click(function () {
+        $("#item").val();
+        $("#cost").val();
+
+    });
+});
+function inTake()
+
+
+$(function(){
+   var size= $("#t-shirt").val();
+
+
+    
+    var cost={
+        large:300,medium:250,small:200
+    }
+})
