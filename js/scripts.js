a8aa99d80f4a876b9e999f70b734d8de1d818737
254jack <taixonj@gmail.com>
2021-11-09 22:01:29 +0300

    add all changes to gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,39 @@ $(function() {
     });
 });
 $(function() {
+    $("#allp").click(function() {
+        $(".check-all").prop('checked', true);
 
+        $(".imgy div").show();
+        $(".images div").show();
+        $("#child").show();
+    });
 });
+
+$(document).ready(function() {
+    $("#men").click(function() {
+        if ($("#men").is(":checked"))
+            $(".imgy div").show();
+        else $(".imgy div").hide();
+    });
+});
+
+$(document).ready(function() {
+    $("#women").click(function() {
+        if ($("#women").is(":checked"))
+            $(".images div").show();
+        else $(".images div").hide();
+    });
+});
+$(document).ready(function() {
+    $("#children").click(function() {
+        if ($("#children").is(":checked"))
+            $("#child").show();
+        else $("#child").hide();
+    });
+});
+$(function() {
+    $("#men,#women").prop('checked', true)
+    $(".imgy div").show();
+    $(".images div").show();
+})
